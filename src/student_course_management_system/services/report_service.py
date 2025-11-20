32f1d44fcb3ecfd36b07f287b02013b41e32e6bd
JoeBright1619 <brightcollin1619@gmail.com>
2025-11-20 14:39:52 +0200

    fix: Change generate method to generate_report as ReportBase is structured

--- src/student_course_management_system/services/report_service.py
+++ src/student_course_management_system/services/report_service.py
@@ -2,7 +2,7 @@ from ..models.report_base import ReportBase
 
 
 class SummaryReport(ReportBase):
-    def generate(self, enrollments):
+    def generate_report(self, enrollments):
         total = len(enrollments)
         courses = {e.course.course_code for e in enrollments}
         students = {e.student.student_id for e in enrollments}
